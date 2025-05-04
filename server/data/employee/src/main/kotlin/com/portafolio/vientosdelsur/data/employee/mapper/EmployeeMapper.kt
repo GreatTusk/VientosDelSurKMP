e99759669851036f5222989466dce8875caa3a81
ArielCamposs <campos25ariel@gmail.com>
2025-05-03 23:43:49 -0400

    1er commit

--- server/data/employee/src/main/kotlin/com/portafolio/vientosdelsur/data/employee/mapper/EmployeeMapper.kt
+++ server/data/employee/src/main/kotlin/com/portafolio/vientosdelsur/data/employee/mapper/EmployeeMapper.kt
@@ -4,5 +4,24 @@ import com.portafolio.vientosdelsur.data.employee.entity.EmployeeDao
 import com.portafolio.vientosdelsur.shared.dto.EmployeeDto
 
 internal fun EmployeeDao.toEmployeeDto() = EmployeeDto(
-    id = id.value
+    id = id.value,
+    firstName = firstName,
+    lastName = lastName,
+    phoneNumber = phoneNumber,
+    dayOff = dayOff?.toDayOfWeek() ?: 7,
+    hireDate = hireDate.toString(),
+    occupation = occupation
 )
+
+private fun String.toDayOfWeek(): Int {
+    return when (this) {
+        "Lunes".lowercase() -> 1
+        "Martes".lowercase() -> 2
+        "Miércoles".lowercase() -> 3
+        "Jueves".lowercase() -> 4
+        "Viernes".lowercase() -> 5
+        "Sábado".lowercase() -> 6
+        "Domingo".lowercase() -> 7
+        else -> error("Invalid day of the week: $this")
+    }
+}
--- shared/src/commonMain/kotlin/com/portafolio/vientosdelsur/shared/dto/EmployeeDto.kt
+++ shared/src/commonMain/kotlin/com/portafolio/vientosdelsur/shared/dto/EmployeeDto.kt
@@ -4,5 +4,12 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class EmployeeDto(
-    val id: Int
+    val id: Int,
+    val firstName: String,
+    val lastName: String,
+    val phoneNumber: String,
+    val dayOff: Int,
+    val hireDate: String,
+    val occupation: String,
+
 )
