f45d59bb8c1b1dddd7e16fe3948f3817b5cb0329
f776 <fern.belmar@duocuc.cl>
2025-05-11 17:04:57 -0400

    refactor DBRoomRepository to streamline room selection logic and remove unused imports

--- server/data/room/src/main/kotlin/com/portafolio/vientosdelsur/data/room/repository/DBRoomRepository.kt
+++ server/data/room/src/main/kotlin/com/portafolio/vientosdelsur/data/room/repository/DBRoomRepository.kt
@@ -7,7 +7,6 @@ import com.portafolio.vientosdelsur.core.database.entity.room.RoomStatus
 import com.portafolio.vientosdelsur.core.database.entity.room.RoomStatusTable
 import com.portafolio.vientosdelsur.core.database.entity.room.RoomTable
 import com.portafolio.vientosdelsur.core.database.entity.work.HousekeeperShiftRoomTable
-import com.portafolio.vientosdelsur.core.database.entity.work.WorkShiftEntity
 import com.portafolio.vientosdelsur.core.database.entity.work.WorkShiftTable
 import com.portafolio.vientosdelsur.core.database.util.suspendTransaction
 import com.portafolio.vientosdelsur.data.room.mapper.toRoom
@@ -18,7 +17,6 @@ import com.portafolio.vientosdelsur.shared.domain.RoomState
 import kotlinx.datetime.LocalDate
 import org.jetbrains.exposed.sql.SortOrder
 import org.jetbrains.exposed.sql.and
-import org.jetbrains.exposed.sql.mapLazy
 import org.jetbrains.exposed.sql.selectAll
 
 internal object DBRoomRepository : RoomRepository {
@@ -36,15 +34,11 @@ internal object DBRoomRepository : RoomRepository {
         date: LocalDate
     ): Result<List<RoomState>, DataError.Remote> = suspendTransaction {
         return@suspendTransaction try {
-            val workShiftId = WorkShiftEntity.find {
-                (WorkShiftTable.employeeId eq housekeeperId) and
-                        (WorkShiftTable.date eq date)
-            }.mapLazy { it.id.value }.firstOrNull() ?: return@suspendTransaction Result.Empty
-
             val rooms = HousekeeperShiftRoomTable
                 .innerJoin(RoomTable)
+                .innerJoin(WorkShiftTable)
                 .selectAll()
-                .where { HousekeeperShiftRoomTable.wordShiftId eq workShiftId }
+                .where { (WorkShiftTable.employeeId eq housekeeperId) and (WorkShiftTable.date eq date) }
                 .map { row ->
                     val roomId = row[RoomTable.id].value
                     val room = RoomEntity[roomId].toRoom()
@@ -53,7 +47,6 @@ internal object DBRoomRepository : RoomRepository {
                         .selectAll()
                         .where { RoomStatusTable.roomId eq roomId }
                         .orderBy(RoomStatusTable.updatedAt to SortOrder.DESC)
-                        .limit(1)
                         .firstOrNull()
 
                     RoomState(
